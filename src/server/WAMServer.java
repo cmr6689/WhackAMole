626380790ee635b6186464f71aa9c45920b7bc1e
cmr6689 <cmr6689@g.rit.edu>
2019-04-12 12:19:45 -0400

    started server implementation

--- src/server/WAMServer.java
+++ src/server/WAMServer.java
@@ -1,4 +1,47 @@
 package server;
 
-public class WAMServer {
+import common.WAMProtocol;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WAMServer implements WAMProtocol {
+    private ServerSocket serverSocket;
+    private int rows;
+    private int columns;
+    private int numPlayers = 1;
+    private int runTime;
+
+    public WAMServer(int port, int rows, int columns, int numPlayers, int runTime) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.rows = rows;
+        this.columns = columns;
+        if (numPlayers > 1) this.numPlayers = numPlayers;
+        this.runTime = runTime;
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 5) {
+            System.out.println("Usage: java WAMServer <port>, <rows>, <columns>, <numPlayers>, <runTime>");
+            System.exit(1);
+        }
+        WAMServer server = new WAMServer(Integer.parseInt(args[0]),
+                Integer.parseInt(args[1]), Integer.parseInt(args[2]),
+                Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+    }
+
+    public void run() {
+        try {
+            System.out.println("Waiting for player one...");
+            Socket playerOneSocket = serverSocket.accept();
+            WAMPlayer playerOne = new WAMPlayer();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 }
