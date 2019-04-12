5b1aa5ea79b8c18527cf6e25b1cb1ccc8d9b616a
Mike M <mm8476@rit.edu>
2019-04-12 13:37:24 -0400

    network client methods

--- src/client/gui/WAMNetworkClient.java
+++ src/client/gui/WAMNetworkClient.java
@@ -6,6 +6,7 @@ import server.WAMGame;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class WAMNetworkClient {
@@ -13,6 +14,7 @@ public class WAMNetworkClient {
     private Scanner networkIn;
     private PrintStream networkOut;
     private WAMBoard board;
+    private boolean go;
 
     public WAMNetworkClient(String host, int port, WAMBoard board) throws Exception {
         try {
@@ -31,5 +33,60 @@ public class WAMNetworkClient {
 
     public void startListener(){
         //new Thread(() -> this.run()).start();
+    }
+
+    public void stop(){
+        this.go = false;
+    }
+
+    public void close(){
+        try{
+            this.clientSocket.close();
+        }
+        catch(IOException e){
+
+        }
+        //this.board.close;
+    }
+
+    private void run(){
+        while (this.go){
+            try{
+                String request = this.networkIn.next();
+                String arguments = this.networkIn.nextLine().trim();
+                //networkOut.println("");
+                switch (request) {
+                    case WAMProtocol.WELCOME:
+                        continue;
+                    case WAMProtocol.ERROR:
+                        continue;
+                    case WAMProtocol.GAME_LOST:
+                        continue;
+                    case WAMProtocol.GAME_TIED:
+                        continue;
+                    case WAMProtocol.GAME_WON:
+                        continue;
+                    case WAMProtocol.MOLE_DOWN:
+                        continue;
+                    case WAMProtocol.MOLE_UP:
+                        continue;
+                    case WAMProtocol.SCORE:
+                        continue;
+                    case WAMProtocol.WHACK:
+                        continue;
+                    default:
+                        System.err.println("unknown request");
+                        this.stop();
+                        break;
+                }
+            }
+            catch(NoSuchElementException nse){
+                this.stop();
+            }
+            catch(Exception e){
+                this.stop();
+            }
+        }
+        this.close();
     }
 }
