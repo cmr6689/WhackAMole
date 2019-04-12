f3892543c555007f8f926f3f0e8c1977af36eb71
Mike M <mm8476@rit.edu>
2019-04-12 13:10:10 -0400

    create buttons in gui

--- src/client/gui/WAMGUI.java
+++ src/client/gui/WAMGUI.java
@@ -2,11 +2,14 @@ package client.gui;
 
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import server.WAMGame;
@@ -17,9 +20,9 @@ public class WAMGUI extends Application {
 
     private GridPane boardpane = new GridPane();
     private WAMGame game;
-    private Scene scene;
     private WAMBoard board;
     private WAMNetworkClient client;
+    private BorderPane window = new BorderPane();
 
 
     public void start(Stage stage) throws Exception {
@@ -27,18 +30,32 @@ public class WAMGUI extends Application {
         for (int i = 0; i < boardarr.length; i++) {
             for (int x = 0; x < boardarr[i].length; x++) {
                 boardarr[i][x] = new Button();
-                Image image = new Image(getClass().getResourceAsStream("empty.png"));
-                boardarr[i][x].setGraphic(new ImageView(image));
+                //Image image = new Image(getClass().getResourceAsStream("empty.png"));
+                //boardarr[i][x].setGraphic(new ImageView(image));
                 //boardarr[i][x].setOnAction(newSendMove(i));
                 //boardarr[i][x].
+                Border border = new Border(new BorderStroke(Color.BLACK,
+                        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
+                boardarr[i][x].setBorder(border);
+                Background background = new Background(new BackgroundFill(Color.DARKOLIVEGREEN, CornerRadii.EMPTY, Insets.EMPTY));
+                boardarr[i][x].setBackground(background);
                 boardpane.add(boardarr[i][x], i, x);
             }
         }
+
+        window.setCenter(boardpane);
+        Scene scene = new Scene(window);
+        stage.setScene(scene);
+        stage.setTitle("WackAMole Game");
+        stage.sizeToScene();
+        stage.setResizable(false);
+        stage.show();
+
     }
 
     @Override
     public void init() throws Exception {
-        try {
+        /*try {
             List<String> args = getParameters().getRaw();
 
             String host = args.get(0);
@@ -51,7 +68,7 @@ public class WAMGUI extends Application {
         catch(NumberFormatException e){
             System.err.println(e);
             throw new RuntimeException(e);
-        }
+        }*/
     }
 
     public static void main(String[] args){
