bf6cacee6fcc3a7cfc62b59ed6498a6dacc70a03
cmr6689 <cmr6689@g.rit.edu>
2019-04-12 13:09:24 -0400

    created basic player class

--- src/server/WAMPlayer.java
+++ src/server/WAMPlayer.java
@@ -1,4 +1,30 @@
 package server;
 
-public class WAMPlayer {
+import common.WAMProtocol;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class WAMPlayer implements WAMProtocol {
+    private Socket socket;
+    private Scanner in;
+    private PrintStream out;
+
+    public WAMPlayer(Socket socket) {
+        this.socket = socket;
+        try {
+            this.in = new Scanner(socket.getInputStream());
+            this.out = new PrintStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
+        out.println("CONNECT");
+    }
+
+
 }
