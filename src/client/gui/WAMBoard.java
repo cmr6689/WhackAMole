a53f0cdc3e5a9badc5017b83c1ca21b6b69d898b
Mike M <mm8476@rit.edu>
2019-04-14 19:40:36 -0400

    comments for wamboard methods

--- src/client/gui/WAMBoard.java
+++ src/client/gui/WAMBoard.java
@@ -10,6 +10,11 @@ public class WAMBoard {
     private final int cols;
     private final int rows;
 
+    /**
+     * WAMBoard constructor, organizes rows into Mole's via a 2d array for the board
+     * @param cols - number of columns to create, called from welcome method
+     * @param rows - number of rows to create, gets info from welcome method
+     */
     public WAMBoard(int cols, int rows){
         this.cols = cols;
         this.rows = rows;
@@ -23,24 +28,44 @@ public class WAMBoard {
         }
     }
 
+    /**
+     * add an observer to the board
+     * @param observer - observer to add
+     */
     public void addObserver(Observer<WAMBoard> observer){
         this.observers.add(observer);
     }
 
+    /**
+     * alert observers about an update. loop through each of the observers in the list
+     */
     private void alertObservers(){
         for(Observer<WAMBoard> obS: this.observers){
             obS.update(this);
         }
     }
 
+    /**
+     * close connection and alert observers of the closed connection
+     */
     public void close() {
         alertObservers();
     }
 
+    /**
+     * get the mole from a specific row/col and return it
+     * @param col - col #
+     * @param row - row #
+     * @return the mole that was located
+     */
     public Mole getContents(int col, int row) {
         return this.board[col][row];
     }
 
+    /**
+     * toggle the mole up, based on the mole number thats passed into the method
+     * @param numMole - mole number
+     */
     public void moleUp(int numMole) {
         for (int col = 0; col < this.cols; col++) {
             for (int row = 0; row < rows; row++) {
@@ -52,6 +77,10 @@ public class WAMBoard {
         alertObservers();
     }
 
+    /**
+     * toggle the mole down, based on the mole number thats passed into the method
+     * @param numMole - mole number
+     */
     public void moleDown(int numMole) {
         for (int col = 0; col < this.cols; col++) {
             for (int row = 0; row < rows; row++) {
