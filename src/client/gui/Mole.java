741f44bcf8c26f2f3bc2c6c72cbc71e85e49db85
cmr6689 <cmr6689@g.rit.edu>
2019-04-14 20:26:34 -0400

    added comments to mole class

--- src/client/gui/Mole.java
+++ src/client/gui/Mole.java
@@ -1,26 +1,51 @@
 package client.gui;
 
+/**
+ * Class that represents the moles with an integer
+ * id and a boolean value representing up or down
+ * @author Cameron Riu
+ * @author Michael Madden
+ */
 public class Mole {
     private int id;
     private boolean up;
 
+    /**
+     * Constructor to make a mole
+     * @param id - id of the mole
+     * @param up - if true then the mole is up
+     */
     public Mole(int id, boolean up) {
         this.id = id;
         this.up = false;
     }
 
+    /**
+     * Return the mole's id
+     * @return id number
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * if the mole is up
+     * @return true if mole is up
+     */
     public boolean isUp() {
         return this.up;
     }
 
+    /**
+     * Set the mole to be up
+     */
     public void moleUp() {
         this.up = true;
     }
 
+    /**
+     * Set the mole to be down
+     */
     public void moleDown() {
         this.up = false;
     }
