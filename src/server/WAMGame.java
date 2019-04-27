ceb4497b05cea324fc0d7242f525c650d8fe9e3a
Mike M <mm8476@rit.edu>
2019-04-27 17:40:38 -0400

    add board and helper methods

--- src/server/WAMGame.java
+++ src/server/WAMGame.java
@@ -1,14 +1,18 @@
 package server;
 
+import client.gui.WAMBoard;
+
 public class WAMGame implements Runnable {
     private WAMPlayer[] players;
     private int runTime;
     private WAMServer server;
+    private WAMBoard board;
 
     public WAMGame(WAMPlayer[] players, int runTime, WAMServer server) {
         this.players = players;
         this.runTime = runTime;
         this.server = server;
+        this.board = server.getBoard();
     }
 
     @Override
@@ -16,6 +20,7 @@ public class WAMGame implements Runnable {
         boolean go = true;
         while (true) {
             // lets pick some random mole and toggle it
+            //server.ge
 
         }
     }
